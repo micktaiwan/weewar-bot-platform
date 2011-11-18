f21df2f3214310b7e64507befffceb2ca5c546b8
Mickael Faivre-Maçon <faivrem@gmail.com>
2011-11-18 01:58:25 +0100

    starting to read a map

--- map.rb
+++ map.rb
@@ -3,17 +3,28 @@ require 'utils'
 # = Weewar map
 class Map
 
-  def initialize(game_id)
-    @game_id = game_id
-    get(game_id)
+  attr_reader :name, :width, :height
+
+  def initialize
+    #@game_id = game_id
+    #get(game_id)
   end
 
   # @return [string] xml representing the game map
-  def get
+  def get(game_id)
     response = Utils.get("api1/map/#{@game_id}")
-    raise "Could not get map. See logs." if(response.code!=200)
-    puts response.body
-    @width = @height = 0
+    raise "Could not get map: #{r.message}" if(response.code!=200)
+    parse(r.body)
+  end
+
+  def parse(xml)
+    doc = REXML::Document.new(xml)
+    @name   = doc.elements['map/name'].text
+    @width  = doc.elements['map/width'].text.to_i
+    @height = doc.elements['map/height'].text.to_i
+    #tag([]) do |games|
+    #  doc.elements.each('game/name') { |g| games << g.text.to_i }
+    #end
   end
 
 end
